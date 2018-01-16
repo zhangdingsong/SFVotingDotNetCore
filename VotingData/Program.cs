1be3d4a3f106440f2fcbc0c98b0d1059a3ce0fde
Dingsong Zhang <dizha@microsoft.com>
2018-01-16 15:35:16 +0800

    add author info

--- VotingData/Program.cs
+++ VotingData/Program.cs
@@ -1,6 +1,7 @@
 ﻿// ------------------------------------------------------------
 //  Copyright (c) Microsoft Corporation.  All rights reserved.
 //  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+//  Dingsong Zhang @ MSFT
 // ------------------------------------------------------------
 
 namespace VotingData
